0fc0038a0c78ab3a2e882de08e4b9cb9835e3a8f
João Victor <j2003.victir@gmail.com>
2024-02-06 17:06:21 -0300

    feat: add language json files and translations

---
+++ src/utils/lang/translations.ts
@@ -0,0 +1,9 @@
+import en from "./en.json";
+import ptBr from "./pt-br.json";
+
+export const translations = { en, "pt-br": ptBr };
+
+export const langagues = [
+  { name: "en", title: "English", flag: "us" },
+  { name: "pt-br", title: "Português Brasileiro", flag: "br" },
+];
